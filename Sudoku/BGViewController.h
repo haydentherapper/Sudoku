fa959ccc95d9acd6d58088e346dfdbfbf7367a45
Sarah Gilkinson <sgilkinson@hmc.edu>
2014-09-11 14:09:27 -0700

    Initial Commit

---
+++ Sudoku/BGViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGViewController.h
+//  Sudoku
+//
+//  Created by Sarah Gilkinson on 9/11/14.
+//  Copyright (c) 2014 Blauzvern Gilkinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGViewController : UIViewController
+
+@end
---
+++ Sudoku/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sudoku
+//
+//  Created by Sarah Gilkinson on 9/11/14.
+//  Copyright (c) 2014 Blauzvern Gilkinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BGAppDelegate class]));
+    }
+}
