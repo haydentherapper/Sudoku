3fcc83f55bbcb4ccf12ac9ceffb6aea7a616505c
Hayden Blauzvern <hblauzvern@gmail.com>
2014-09-19 12:07:19 -0700

    Added NumPadView

---
+++ Sudoku/BKNumPadView.h
@@ -0,0 +1,13 @@
+//
+//  BKNumPadView.h
+//  Sudoku
+//
+//  Created by Hayden Blauzvern on 9/19/14.
+//  Copyright (c) 2014 Blauzvern Gilkinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BKNumPadView : UIView
+
+@end
---
+++ Sudoku/BKNumPadView.m
@@ -0,0 +1,31 @@
+//
+//  BKNumPadView.m
+//  Sudoku
+//
+//  Created by Hayden Blauzvern on 9/19/14.
+//  Copyright (c) 2014 Blauzvern Gilkinson. All rights reserved.
+//
+
+#import "BKNumPadView.h"
+
+@implementation BKNumPadView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
